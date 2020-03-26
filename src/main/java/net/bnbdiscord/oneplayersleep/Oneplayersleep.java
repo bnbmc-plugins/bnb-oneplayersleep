5f848e81dff1456e656c9d3639ffe28fac3b52a7
BananaFalls <BananaFallsYT@gmail.com>
2020-03-25 20:00:22 -0700

    Inital Import

---
+++ src/main/java/net/bnbdiscord/oneplayersleep/Oneplayersleep.java
@@ -0,0 +1,17 @@
+package net.bnbdiscord.oneplayersleep;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Oneplayersleep extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
